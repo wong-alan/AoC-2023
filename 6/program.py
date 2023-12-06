fce28af2eb8b4d38742fcfdc1e616ec249ca6513
Alan Wong <misteralanwong@gmail.com>
2023-12-06 02:08:19 -0800

    Improve day 6 part 2

--- 6/program.py
+++ 6/program.py
@@ -26,13 +26,16 @@ def part_two(text):
     time = int(text.readline().replace(" ", "").split(":")[1])
     distance = int(text.readline().replace(" ", "").split(":")[1])
 
-    ways_to_win = 0
+    ways_to_lose = 0
+    # Start at 1 because we can't hold the button for 0 time
     for t in range(1, time):
         dist = t * (time - t)
-        if dist > distance:
-            ways_to_win += 1
-
-    return ways_to_win
+        if dist < distance:
+            ways_to_lose += 1
+        else:
+            break
+    # -1 because we can't hold the button for the full time
+    return time - 1 - (ways_to_lose * 2)
 
 
 if __name__ == "__main__":
